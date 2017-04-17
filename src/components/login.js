63d5014274ac0bb585686df5908c7de1841a66dd
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-04-17 16:09:36 -0400

    add login and navbar components

--- src/components/login.js
+++ src/components/login.js
@@ -2,7 +2,29 @@ import React, { Component } from 'react';
 
 class Login extends Component {
     render(){
-        return <div> hello world </div>
+        return (
+        <div className="container">
+            <form >
+                <div className="row">
+                    <div className="form-group col-xs-5">
+                        <label>Username</label>
+                        <input className="form-control" type="text" placeholder="Enter Username" required/>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="form-group col-xs-5">
+                        <label>Password</label>
+                        <input className="form-control" type="password" placeholder="Enter Password" required/>
+                        <a href="#" className="text-small">New? <b>Sign up</b></a>
+                    </div>
+                </div>
+                <button className="btn btn-info">Login</button>
+            </form>
+
+        </div>
+        )
+
     }
 };
 
